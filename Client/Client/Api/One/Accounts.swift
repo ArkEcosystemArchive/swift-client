68cf38f9d1beece947e9cb377fcab695237bf82a
ItsANameToo <itsanametoo@outlook.com>
2018-07-12 23:35:56 +0200

    chore: update file headers with license information

--- Client/Client/Api/One/Accounts.swift
+++ Client/Client/Api/One/Accounts.swift
@@ -1,9 +1,10 @@
 //
-//  Accounts.swift
-//  Client
+// This file is part of Ark Swift Client.
 //
-//  Created by ItsANameToo on 10-07-18.
-//  Copyright © 2018 ARK Ecosystem. All rights reserved.
+// (c) Ark Ecosystem <info@ark.io>
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
 //
 
 import Alamofire
--- Client/ClientTests/ClientTests.swift
+++ Client/ClientTests/ClientTests.swift
@@ -1,9 +1,10 @@
 //
-//  ClientTests.swift
-//  ClientTests
+// This file is part of Ark Swift Client.
 //
-//  Created by ItsANameToo on 10-07-18.
-//  Copyright © 2018 ARK Ecosystem. All rights reserved.
+// (c) Ark Ecosystem <info@ark.io>
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
 //
 
 import XCTest
