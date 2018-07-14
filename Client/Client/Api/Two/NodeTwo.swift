9d8ce5f966bb3897cb6f17ef4cf5b9af5ab7af41
ItsANameToo <itsanametoo@outlook.com>
2018-07-15 01:05:15 +0200

    feat: v2 node endpoint

---
+++ Client/Client/Api/Two/NodeTwo.swift
@@ -0,0 +1,43 @@
+//
+// This file is part of Ark Swift Client.
+//
+// (c) Ark Ecosystem <info@ark.io>
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+//
+
+import Foundation
+
+extension Two {
+    class Node {
+        
+        private let client: ArkClient
+        private let apiGetHandler: ApiGetHandler
+        private var endpoint: String {
+            get {
+                return client.host + "/node"
+            }
+        }
+        
+        public init(client: ArkClient, _ apiGetHandler: @escaping ApiGetHandler = handleApiGet) {
+            self.client = client
+            self.apiGetHandler = apiGetHandler
+        }
+        
+        /// Retrieves the status
+        public func status(completionHandler: @escaping (Dictionary<String, Any>?) -> Void) {
+            apiGetHandler(endpoint + "/status", [:], completionHandler)
+        }
+        
+        /// Retrieves the node syncing status
+        public func syncing(completionHandler: @escaping (Dictionary<String, Any>?) -> Void) {
+            apiGetHandler(endpoint + "/syncing", [:], completionHandler)
+        }
+        
+        /// Retrieves the configuration
+        public func configuration(completionHandler: @escaping (Dictionary<String, Any>?) -> Void) {
+            apiGetHandler(endpoint + "/configuration", [:], completionHandler)
+        }
+    }
+}
