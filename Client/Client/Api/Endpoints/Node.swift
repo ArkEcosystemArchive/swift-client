02465d2914074c31e830b1ecb8f4938a25458070
ItsANameToo <itsanametoo@protonmail.com>
2019-07-11 11:35:49 +0200

    chore: update Alamofire and fix whitespace issue

--- Client/Client/Api/Endpoints/Node.swift
+++ Client/Client/Api/Endpoints/Node.swift
@@ -36,7 +36,7 @@ public class Node {
     public func configuration(completionHandler: @escaping ([String: Any]?) -> Void) {
         apiGetHandler("\(endpoint)/configuration", [:], completionHandler)
     }
-    
+
     /// Retrieves the configuration
     public func fees(completionHandler: @escaping ([String: Any]?) -> Void) {
         apiGetHandler("\(endpoint)/fees", [:], completionHandler)
